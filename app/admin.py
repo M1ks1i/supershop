69e6cc0d12af5a6570c74627537ca628740b35cd
M1ks1i <krilica19@gmail.com>
2026-02-17 20:05:18 +0100

    added settungs

--- app/admin.py
+++ app/admin.py
@@ -6,4 +6,43 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['name','price','in_stock','created_at']
     list_filter = ['in_stock','created_at']
     search_fields =  ['discription','name']
-    readonly_fields = ['created_at']
+    readonly_fields = ['created_at','colored']
+    list_editable = ['price','in_stock']
+    list_per_page = 24
+    ordering = ['-created_at']
+    date_hierarchy = 'created_at'
+    fieldsets = (
+        (
+            'Main Информейшн',
+            {'fields':('name','discription')}
+        ),
+        (
+            'Цена и Наличие',
+            {'fields':('price','in_stock')}
+        ),
+        (
+            'Дополнительная Information',
+            {
+                'fields':('created_at',),
+                'classes':('collapse',)
+            }
+        )
+    )
+
+    def colored(self,obj):
+        if obj.in_stock :
+            return '✅В наличии✅'
+        return '❌Нет в наличии❌'
+    colored.short_description = 'status'
+
+    action = ['make_in_stock','make_out_off_stock']
+
+    def make_in_stock(self, request, query_set):
+        query_set.update(in_stock = True)
+        self.message_user(request,"Товар обозначен 'В наличии'" )
+    make_in_stock.short_description = 'В наличии'
+
+    def make_out_off_stock(self, request, query_set):
+        query_set.update(in_stock = False)
+        self.message_user(request,"Товар обозначен 'Не в наличии'" )
+    make_out_off_stock.short_description = 'Не в наличии'
